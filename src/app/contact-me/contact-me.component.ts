b2e750f8630063467e9079ddd6bbf9099581184d
unknown <supriyamohan98@gmail.com>
2026-02-18 19:54:50 +0530

    initial commit

---
+++ src/app/contact-me/contact-me.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-me.component.html',
+  styleUrl: './contact-me.component.css'
+})
+export class ContactMeComponent {
+  mailId = ' supriyamohan98@gmail.com';
+  at = '@';
+}
